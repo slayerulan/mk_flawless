5ac8798ed6ace8ee82b2b7cbd1e52a0991e6f7bd
karambaq <0maximq@gmail.com>
2018-06-16 01:03:51 +0300

    :tada:

--- src/main.py
+++ src/main.py
@@ -5,11 +5,14 @@ from get_data import get_data
 
 
 
-sended = deque(maxlen=10)
+sended = deque(maxlen=15)
 
 while True:
     for g in get_data():
         if g.id not in sended and g.is_fw_coef():
-            print(g)
+            print(f"Sending: {g}")
             sended.append(g.id)
             sleep(30)
+        else:
+            if g.id not in sended:
+                print(g)
