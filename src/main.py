0e7f8416d272a7fae909aa61e1a44da531523e18
karambaq <0maximq@gmail.com>
2018-06-16 01:06:34 +0300

    :tada:

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from time import sleep
 from collections import deque
 
 from get_data import get_data
+from send_to_ch import send_msg
 
 
 
@@ -11,6 +12,7 @@ while True:
     for g in get_data():
         if g.id not in sended and g.is_fw_coef():
             print(f"Sending: {g}")
+            send_msg(str(g))
             sended.append(g.id)
             sleep(30)
         else:
